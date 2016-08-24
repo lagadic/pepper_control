fdce97029351beeece19c94dee2286ead7cac6af
jokla <giovanni.claudio@inria.fr>
2016-08-24 09:01:19 +0200

    added new velocity controller: if one joint is in limit the joints are stopped.

--- control.cpp
+++ control.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <sstream> 
 #include <qi/clock.hpp>
+#include <alcommon/alproxy.h>
 #include <boost/chrono/chrono_io.hpp>
 #include <control.hpp>
 
+#define FLAGACC		1	// Axe en acceleration
+#define FLAGCTE		2 // Axe en vitesse constante
+#define FLAGDEC		3	// Axe en deceleration
+#define FLAGSTO		4	// Axe stoppe
+
+#define DELTAQMIN	0.0001	// Delta Q minimum (rad)
+#define OFFSET_BUTEE    0 //0.01	// on s'arrete un peu avant les butees
+
+
 Control::Control(qi::SessionPtr session)
-  : m_session(session), m_prev(), m_task(), m_vel(), m_pos(), m_jointNames(),lock_(), m_taskStarted(false), m_firstTimeTask(true)
+  : m_session(session), m_prev(), m_task(), m_vel(), m_pos(),
+    m_jointNames(),lock_(), m_taskStarted(false), m_firstTimeTask(true),
+    Status(), deltaQmax(), deltaQ(), Qacc(), Qacc_sav(), ConsFin(), SigneDep(),
+    PQc(), dist_AD(), pcspeed(), pcspeed_old(), ecart(), FlagSpeed(), vmax(false), flagbutee(false),
+    AccMax(), VitMax(), QMin(), QMax(), period_ms(2)
 {
   m_prev = qi::SteadyClock::now();
   m_motion = m_session->service("ALMotion");
+}
+
+Control::~Control()
+{
+
 }
 
 void Control::start()
@@ -17,14 +36,14 @@ void Control::start()
   std::cout << "Task started " << std::endl;
 }
 
-//void Control::stopJoint()
-//{
-//  lock_.lock();
-//  std::vector<float> zero (m_vel.size(), 0.0);
-//  m_vel = zero;
-//  lock_.unlock();
-//  std::cout << "Stop joints" << std::endl;
-//}
+void Control::stopJoint()
+{
+  lock_.lock();
+  std::vector<float> zero (m_vel.size(), 0.0);
+  m_vel = zero;
+  lock_.unlock();
+  std::cout << "Stop joints" << std::endl;
+}
 
 void Control::stop()
 {
@@ -37,6 +56,78 @@ void Control::stop()
 
 void Control::setDesJointVelocity (std::vector<std::string> jointNames, std::vector<float> vel)
 {
+  lock_.lock();
+  std::vector<std::string> jointNames_prev = m_jointNames;
+  lock_.unlock();
+
+  if (jointNames != jointNames_prev) {
+    // TODO: Call next only if joint names modified
+    size_t njoint = jointNames.size();
+    Status.resize(njoint);
+    deltaQmax.resize(njoint);
+    deltaQ.resize(njoint);
+    Qacc.resize(njoint);
+    Qacc_sav.resize(njoint);
+    ConsFin.resize(njoint);
+    SigneDep.resize(njoint);
+    PQc.resize(njoint);
+    dist_AD.resize(njoint);
+    pcspeed.resize(njoint);
+    pcspeed_old.resize(njoint);
+    ecart.resize(njoint);
+    FlagSpeed.resize(njoint);
+    AccMax.resize(njoint);
+    VitMax.resize(njoint);
+    QMin.resize(njoint);
+    QMax.resize(njoint);
+
+
+    //  for (unsigned int i=0; i<jointNames.size(); i++)
+    //  {
+    //    qi::AnyValue limits = m_motion.call< qi::AnyValue >("getLimits", jointNames[i]);
+    //    //std::cout << limits << std::endl;
+    //    QMin[i] = limits[0].asFloat();
+    //    QMax[i] = limits[1].asFloat();
+    //    VitMax[i] = limits[2].asFloat();
+    //    AccMax[i] = 0.1; // To improve (~10deg in 2 sec)
+    //    std::cout << " jointNames " << jointNames[i] << " " << QMin[i] << " " << QMax[i] << " " << VitMax[i] << std::endl;
+    //  }
+
+    for (unsigned int i=0; i<jointNames.size(); i++)
+    {
+      AL::ALValue limits = m_motion.call< AL::ALValue >("getLimits", jointNames[i]);
+      //std::cout << limits << std::endl;
+      QMin[i] = limits[0][0];
+      QMax[i] = limits[0][1];
+      VitMax[i] = limits[0][2];
+      AccMax[i] = 0.5*8; // To improve (~10deg in 2 sec)
+      std::cout << " jointNames " << jointNames[i] << " " << QMin[i] << " " << QMax[i] << " " << VitMax[i] << std::endl;
+
+      FlagSpeed[i] = false;
+      Status[i] 	= FLAGSTO;
+      deltaQ[i] 	= deltaQmax[i] 	= 0.0;
+      Qacc_sav[i] = Qacc[i] = AccMax[i] * period_ms * period_ms / 1000000.;
+      ConsFin[i] 	= PQc[i] = 0.0; //get_q(i);
+      dist_AD[i] 	= 0.0;
+      pcspeed[i]	= pcspeed_old[i] = 0.0;
+      Status[i]   = FLAGSTO;
+      SigneDep[i]	= 0;
+      ConsFin[i] = 0;
+    }
+
+    m_pos = m_motion.call<std::vector<float> >("getAngles", jointNames, 1);
+
+    for (unsigned int i=0; i<jointNames.size(); i++)
+      PQc[i] = m_pos[i];
+
+  }
+
+  //vmax = false;
+//  m_pos = m_motion.call<std::vector<float> >("getAngles", jointNames, 1);
+
+//  for (unsigned int i=0; i<jointNames.size(); i++)
+//    PQc[i] = m_pos[i];
+
   lock_.lock();
   m_vel = vel;
   m_jointNames = jointNames;
@@ -62,12 +153,215 @@ qi::PeriodicTask::Callback Control::printTime()
 
 void Control::applyJointVelocity()
 {
-
   qi::SteadyClock::time_point now = qi::SteadyClock::now();
 
   lock_.lock();
-  if (m_vel.size()>0 && m_jointNames.size()>0)
+  std::vector<std::string> jointNames = m_jointNames;
+  std::vector<float> vel = m_vel;
+  lock_.unlock();
+
+  if (vel.size()>0 && jointNames.size()>0)
   {
+
+#if 1
+    qi::MicroSeconds us = boost::chrono::duration_cast<qi::MicroSeconds>(now - m_prev);
+    double delta_t =  us.count()/1000000.;
+
+      std::cout << " delta " << delta_t << std::endl;
+
+    /*
+     * Test si changement de consigne.
+     */
+    for (int i=0;i<jointNames.size();i++) {
+      pcspeed[i] = vel[i]; // TODO fuse in same var
+
+      if (pcspeed[i] != pcspeed_old[i]) flagbutee = false;
+
+      if (pcspeed[i] != pcspeed_old[i]) {
+        Qacc[i] = Qacc_sav[i];
+
+        if (pcspeed[i] > VitMax[i]) {
+          pcspeed[i] = VitMax[i];
+          if (vmax == false)
+          {
+            vmax = true;
+          }
+        }
+        else if (pcspeed[i] < (-VitMax[i])) {
+          pcspeed[i] = -VitMax[i];
+          if (vmax == false)
+          {
+            vmax = true;
+          }
+        }
+        else vmax = false;
+
+        if (FlagSpeed[i] == false) {
+          //if (pt_movespeed.FlagSpeed[i] == false) {
+          /* Changement de consigne et non en phase de chang sens */
+
+          if ( Status[i] == FLAGSTO) /* Si arret */
+          {
+            if (pcspeed[i] > 0)
+            {
+              deltaQmax[i] = pcspeed[i]*delta_t;
+              SigneDep[i] = 1;
+              ConsFin[i] = QMax[i] - OFFSET_BUTEE;
+              deltaQ[i] = 0;
+              Status[i] = FLAGACC;
+            }
+            else if (pcspeed[i] < 0)
+            {
+              deltaQmax[i] = - pcspeed[i]*delta_t;
+              SigneDep[i] = -1;
+              ConsFin[i] = QMin[i] + OFFSET_BUTEE;
+              deltaQ[i] = 0;
+              Status[i] = FLAGACC;
+            }
+          }
+          // Si non en arret et changement de sens
+
+          else if ( (pcspeed[i] * SigneDep[i]) < 0) {
+            FlagSpeed[i] = true;
+            Status[i] = FLAGDEC;
+            deltaQmax[i] = 0;
+          }
+          // Pas de changement de sens
+
+          else {	/* Non arret et pas de changement de sens */
+            if ( SigneDep[i] == 1) {
+              if ( pcspeed[i] > pcspeed_old[i])
+                Status[i] = FLAGACC;
+              else  Status[i] = FLAGDEC;
+              deltaQmax[i] = pcspeed[i]*delta_t;
+            }
+            else {
+              if ( pcspeed[i] > pcspeed_old[i])
+                Status[i] = FLAGDEC;
+              else  Status[i] = FLAGACC;
+              deltaQmax[i] = - pcspeed[i]*delta_t;
+            }
+          }
+
+          int n = (int) (deltaQmax[i] / Qacc[i]);
+          dist_AD[i]=n*(deltaQmax[i]-(n+1)*Qacc[i]/2);
+        }
+        pcspeed_old[i] = pcspeed[i];
+      }
+    }
+
+    std::cout << "ConsFin: ";
+    for (int i=0;i<jointNames.size();i++)
+      std::cout << jointNames[i] << "(" << ConsFin[i] << ") ";
+
+    /*
+     * calcul des consignes selon les cas:
+     *		- acceleration
+     *		- deceleration
+     *		- arret
+     */
+
+    for (int i=0;i<jointNames.size();i++) {
+      /*
+       * Securite butee en vitesse constante
+       */
+      ecart[i] = ( ConsFin[i] - PQc[i]) * SigneDep[i];
+      if ((ecart[i] - deltaQmax[i]) <=  dist_AD[i]) {
+        if (dist_AD[i] > 0) {
+          if (!flagbutee) printf("Flagbutee axe %d\n",i);
+          flagbutee = true;
+          for(int k=0;k<jointNames.size();k++)
+          {
+            if (Status[k] != FLAGSTO) Status[k] = FLAGDEC;
+            deltaQmax[k] = 0;
+          }
+        }
+      }
+      /*
+       * Deceleration.
+       */
+      if ( Status[i] == FLAGDEC) {
+        deltaQ[i] -=  Qacc[i];
+        if (deltaQ[i] <=  deltaQmax[i]) {
+          if (deltaQmax[i] < DELTAQMIN)  {
+            Status[i] = FLAGSTO;
+            deltaQ[i] = 0.0;
+            // Test si on etait en phase de changement de sens.
+            if (FlagSpeed[i] == true) {
+              //if (pt_movespeed.FlagSpeed[i] == true) {
+              if (pcspeed[i] > 0) {
+                deltaQmax[i] = pcspeed[i]*delta_t;
+                SigneDep[i] = 1;
+                ConsFin[i] = QMax[i] - OFFSET_BUTEE;
+              }
+              else if (pcspeed[i] < 0) {
+                deltaQmax[i] = -pcspeed[i]*delta_t;
+                SigneDep[i] = -1;
+                ConsFin[i] = QMin[i] + OFFSET_BUTEE;
+              }
+              Status[i] = FLAGACC;
+              FlagSpeed[i] = false;
+
+              int n = (int) (deltaQmax[i] / Qacc[i]);
+              dist_AD[i]=n*(deltaQmax[i]-(n+1)*Qacc[i]/2);
+            }
+          }
+          else if ((deltaQmax[i] > 0) && !flagbutee)  {
+            if (deltaQmax[i] < (deltaQ[i] + 2*Qacc[i])) {
+              deltaQ[i] = deltaQmax[i];
+              Status[i] = FLAGCTE;
+            }
+            else if (!flagbutee) {
+              /* acceleration moins rapide*/
+              deltaQ[i] += (2*Qacc[i]);
+              Status[i] = FLAGACC;
+            }
+          }
+        }
+      }
+      /*
+       * Acceleration.
+       */
+      else if (Status[i] == FLAGACC) {
+        deltaQ[i] += Qacc[i];
+
+        if (deltaQ[i] >= deltaQmax[i]) {
+          deltaQ[i] = deltaQmax[i];
+          Status[i] = FLAGCTE;
+        }
+      }
+      /*
+       * Sinon a vitesse constante increment non change.
+       */
+      PQc[i] += SigneDep[i] * deltaQ[i];
+
+    } /* endfor */
+
+    // Test si un axe arrive pres des butees. Si oui, arret de tous les axes
+    for (int i=0;i<jointNames.size();i++) {
+      float butee = QMin[i] + OFFSET_BUTEE;
+      if (PQc[i] < butee) {
+        for (int j=0;j<jointNames.size();j++) PQc[j] -= SigneDep[j]*deltaQ[j];
+        PQc[i] = butee;
+        printf("Butee axe %d\n",i);
+        break;
+      }
+      butee = (float) (QMax[i] - OFFSET_BUTEE);
+      if (PQc[i] > butee) {
+        for (int j=0;j<jointNames.size();j++) PQc[j] -= SigneDep[j]*deltaQ[j];
+        PQc[i] = butee;
+        printf("Butee axe %d\n",i);
+        break;
+      }
+    }
+    std::cout << "new_pos: ";
+    for (int i=0;i<jointNames.size();i++)
+      std::cout << jointNames[i] << "(" << PQc[i] << ") ";
+    std::cout << std::endl;
+
+    // Apply new position
+    m_motion.async<void>("setAngles", jointNames, PQc, 1.0);
+#else
     //std::cout << "m_vel: " << m_vel[0] << std::endl << m_vel[1] << std::endl  << "m_jointNames" <<   m_jointNames[0] << std::endl<<   m_jointNames[1] << std::endl;
     if (m_firstTimeTask)
     {
@@ -91,10 +385,11 @@ void Control::applyJointVelocity()
 
     m_motion.async<void>("setAngles", m_jointNames, new_pos, 1.0);
     m_pos = new_pos;
+#endif
   }
 
+
   m_prev = now;
-  lock_.unlock();
 }
 
 
@@ -106,7 +401,7 @@ void Control::setTask()
     m_jointNames.clear();
     m_pos.clear();
     m_task.setName("Setvelocity");
-    m_task.setUsPeriod(2000);
+    m_task.setUsPeriod(period_ms * 1000);
     m_task.setCallback(&Control::applyJointVelocity, this);
     m_taskStarted = true;
   }
--- control.hpp
+++ control.hpp
@@ -13,19 +13,44 @@ private:
   qi::SessionPtr m_session;
   qi::SteadyClock::time_point m_prev;
   qi::PeriodicTask m_task;
-  std::vector<float> m_vel;
+  std::vector<float> m_vel; // protected by mutex
   std::vector<float> m_pos;
-  std::vector<std::string> m_jointNames;
+  std::vector<std::string> m_jointNames; // protected by mutex
   qi::AnyObject m_motion;
   boost::mutex lock_;
 
+  int period_ms;
 
   bool m_taskStarted;
   bool m_firstTimeTask;
 
+  std::vector<int>	 Status;	        // Flag d'etat des axes
+  std::vector<float> deltaQmax;        // Increment de consigne maximum
+  std::vector<float> deltaQ;		// Increment de consigne en cours
+  std::vector<float> Qacc;		// Increment d'increment de consigne
+  std::vector<float> Qacc_sav;         // Sauvegarde
+  std::vector<float> ConsFin;    	// Consigne finale (butee)
+  std::vector<int>   SigneDep;	        // Signe du deplacement: +1 = incrementer consigne
+  std::vector<float> PQc;              // Position mesuree et position calculee en rad
+  std::vector<float> dist_AD;		// Distance requise pour acce et decel
+  std::vector<float> pcspeed;		// Recopie des consigne de vitesse
+  std::vector<float> pcspeed_old;
+  std::vector<float> ecart;		// Difference entre consigne et consigne finale
+  std::vector<float> FlagSpeed;
+  bool vmax;
+  bool flagbutee;
+
+  // Constant
+  std::vector<float> AccMax;    // Acceleration maximale
+  std::vector<float> VitMax;    // Vitesses maximales
+  std::vector<float> QMax;	// Butee softs maximale
+  std::vector<float> QMin;	// Butee softs minimale
+
+
 
 public:
   Control(qi::SessionPtr session);
+  virtual ~Control();
 
   std::vector<float> getJointValues (std::vector<std::string> jointNames) const;
   void setDesJointVelocity (std::vector<std::string> jointNames, std::vector<float> vel);
@@ -33,8 +58,8 @@ public:
   qi::PeriodicTask::Callback printTime() ;
   void applyJointVelocity() ;
   void start();
-//  void stopJoint();
+  void stopJoint();
   void stop();
 
 };
-QI_REGISTER_OBJECT(Control, getJointValues, printTime, setDesJointVelocity, start, stop);
+QI_REGISTER_OBJECT(Control, getJointValues, printTime, setDesJointVelocity, start, stop, stopJoint);
